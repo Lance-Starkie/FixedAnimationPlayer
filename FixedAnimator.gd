10a942fc4d4ff1800b64f2037f6eb688f025de1b
Lance Starkie <68128915+Lance-Starkie@users.noreply.github.com>
2024-02-22 13:18:13 -0700

    Update FixedAnimator.gd
    Added save/load

--- FixedAnimator.gd
+++ FixedAnimator.gd
@@ -2,6 +2,9 @@ extends Node
 
 # Constants for the animation system.
 const DEFAULT_SPEED_SCALE = 10080
+const FALL_ANIMATION = "fall"
+const IDLE_ANIMATION = "idle"
+const SPECIAL_ANIMATIONS = [IDLE_ANIMATION, FALL_ANIMATION]
 
 # Exported variables for assigning animation player and resource paths.
 export (NodePath) var animation_player_path = NodePath("../AnimationPlayerLegacy")
@@ -19,8 +22,14 @@ var paused = false
 var looping = false
 var instant_simulation = true
 
-# List of whitelisted functions.
-var allowed_functions = ["shoot_lazer","sheilds"]
+# SECURITY WARNING:
+# Exposing function calls in a P2P game directly to the Godot engine poses significant risks, including potential 
+# arbitrary code execution and data breaches. Ensure strict validation of allowed functions and consider the 
+# security implications of each function exposed. Limit the use of `allowed_functions` to those necessary 
+# and restricted to from any function call arbitration (use of call(input_var: str)), or injection vulnerabilities.
+
+# List of whitelisted functions. Add any functions you use here, don't expose any vulnerabilities here.
+var allowed_functions = ["insert","all_animator_called","functions","here"]
 
 var frame_accumulator = 0
 var playing = false
@@ -30,7 +39,11 @@ func _ready():
 	if not Engine.editor_hint:
 		get_node("../AnimationPlayerLegacy").call_deferred("free")
 
-func play(animation_name = ""):
+func play(animation_name = "", no_simulate = false):
+	if animation_name in SPECIAL_ANIMATIONS:
+		if len(animation_queue) == 0:
+			get_parent().fully_actionable()
+		return
 	# Set default animation_name if not provided or empty.
 	if animation_name == "":
 		animation_name = assigned_animation
@@ -41,7 +54,7 @@ func play(animation_name = ""):
 		if not playing:
 			playing = true
 			return
-		else:#if get_current_animation_length() != current_frame:
+		elif get_current_animation_length() != current_frame:
 			return
 
 	# Set the new animation and play it.
@@ -50,7 +63,8 @@ func play(animation_name = ""):
 	assigned_animation = animation_name
 	current_frame = -1
 	playing = true
-	if instant_simulation:
+	if (instant_simulation and not no_simulate):
+		current_frame += 1
 		run_frame()
 
 func queue(animation_name):
@@ -60,6 +74,9 @@ func queue(animation_name):
 	if current_animation == "":
 		play(animation_name)
 		return
+	elif not animation_name in SPECIAL_ANIMATIONS:
+		animation_queue.erase(IDLE_ANIMATION)
+		animation_queue.erase(FALL_ANIMATION)
 	animation_queue.append(animation_name)
 
 func is_playing():
@@ -67,6 +84,7 @@ func is_playing():
 
 func stop(keep_state: bool = false):
 	# Stop the animation and dequeue the next one.
+	looping = false
 	playing = false
 	current_frame = -1
 	current_animation = ""
@@ -74,8 +92,9 @@ func stop(keep_state: bool = false):
 #		play(animation_queue.pop_front())
 
 func find_keyframes(frame):
-	# Find all keyframes for a given frame in the current animation.
+	# Find all keyframes for each frame in the current animation.
 	var keyframes = []
+	looping = false
 	for track in animation_resource.animation_data[assigned_animation].keys():
 		if track in ["<length>", "<loop>"]:
 			if track == "<loop>":
@@ -100,10 +119,14 @@ func resume():
 	paused = false
 
 func frame_pass():
-	if paused or not playing or current_animation == "":
+	#Iterate frames in animation player
+	if paused:
+		return
+	if not playing or current_animation == "":
+		run_queue()
 		return
 	frame_accumulator += speed_scale
-	if animation_queue.size() > 0:
+	if current_animation in SPECIAL_ANIMATIONS and animation_queue.size() > 0:
 		frame_accumulator = max(DEFAULT_SPEED_SCALE, frame_accumulator)
 	while frame_accumulator >= DEFAULT_SPEED_SCALE:
 		frame_accumulator -= DEFAULT_SPEED_SCALE
@@ -112,23 +135,28 @@ func frame_pass():
 			run_queue()
 		run_frame()
 
+func animation_complete():
+	return current_frame > get_current_animation_length()
+
 func run_queue():
+	playing = false
 	if animation_queue.size() > 0:
 		play(animation_queue.pop_front())
 		if instant_simulation:
 			return
 	elif looping:
 		current_frame = -1
-		play()
+		play('', false)
 		if instant_simulation:
 			return
 	else:
-		stop(false)
+		stop()
 		return
 
-func run_frame():
-	if paused or not playing or current_animation == "":
+func run_frame(force = false):
+	if (paused or not playing or current_animation == "") and not force:
 		return
+	#print(current_animation + ": " + str(current_frame) + " out of " +str(get_current_animation_length()) + " queue: " + str(animation_queue))
 	apply_frame(find_keyframes(current_frame))
 
 func apply_frame(keyframes):
@@ -182,8 +210,10 @@ func compare_keyframes(a, b):
 	return a_index - b_index
 
 func seek(animation_name, frame):
-	play(animation_name)
-	current_frame = clamp(frame, 0, get_current_animation_length())
+	play(animation_name, false)
+	current_frame = clamp(frame, -1, get_current_animation_length())
+	#if animation_complete():pass# run_queue()
+	#elif current_frame != -1: run_frame()
 
 func get_current_frame():
 	return current_frame
@@ -214,3 +244,25 @@ func set_animation_queue(new_queue):
 func clear_queue():
 	set_animation_queue([])
 
+func _save():
+	var gamestate = {}
+	var restored_animation_queue = []
+	for index in animation_queue:
+		restored_animation_queue.append(subloads.AnimationMap.ANIMATION_MAP.keys()[index])
+	gamestate["animation_queue"] = restored_animation_queue
+	gamestate["animation_state"] = current_animation
+	var frame = get_current_frame()
+	gamestate["animation_frame"] = frame
+	return gamestate
+
+func _load(gamestate: Dictionary):
+	var restored_animation_queue = []
+	for index in gamestate["animation_queue"]:
+		restored_animation_queue.append(subloads.AnimationMap.ANIMATION_MAP.keys()[index])
+
+	animation_player.set_animation_queue(restored_animation_queue)
+
+	current_animation = gamestate["animation_state"]
+
+	if gamestate["animation_frame"] > 0:
+		animation_player.seek(current_animation, (gamestate["animation_frame"])-1)
